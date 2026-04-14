d076d99ee1d39e104180b9950ec8687738fd7043
progj <joddarprakash@gmail.com>
2026-04-15 00:59:22 +0530

    repalce fetch with axios

--- hooks/useVehicleHistory.ts
+++ hooks/useVehicleHistory.ts
@@ -1,18 +1,13 @@
 import { useEffect } from "react";
 import { useHistoryStore } from "@/store/history-store";
-import { BASE_URL } from "@/lib/api";
+import { api } from "@/lib/api";
 
 export function useVehicleHistory(vehicleNo: string, date: string) {
   const setPoints = useHistoryStore((s) => s.setPoints);
 
   useEffect(() => {
     async function load() {
-      const res = await fetch(
-        BASE_URL + `/location/history/${vehicleNo}/${date}`,
-      );
-
-      const data = await res.json();
-
+      const { data } = await api.get(`/location/history/${vehicleNo}/${date}`);
       setPoints(data);
     }
 
